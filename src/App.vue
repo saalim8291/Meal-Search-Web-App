f7511c83efa1dca20f8fde1d1004d8b05be5a900
Saalim Bhatkar <saalimbhatkar786@gmail.com>
2024-05-16 22:14:02 +0530

    route added

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 </script>
-
 <template>
+  <router-view />test
 </template>
 
 <style scoped>
@@ -11,9 +11,11 @@
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
